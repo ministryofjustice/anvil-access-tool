e9f9f18b8a6ae44eccb79f010b4e14d15a6f9463
hbutchermoj <hannah.butcher2@noms.gsi.gov.uk>
2018-07-16 11:32:18 +0000

    Initial design

--- server.R
+++ server.R
@@ -8,19 +8,102 @@
 #
 
 library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+library(shinyjs)
+library(data.table)
+
+fields<-c("first_name","surname","prison","role","quantum_id","apps_needed")
+errorMes<-c(0,0,0,0,0,0,0)
+foundErrors<-0
+
+saveData<-function(data){
+  #filePath<-"/home/hbutchermoj/anvil-access-app/anvil-access-tool"
+  #fileName<-"test.csv"
+  
+  #write.csv(x=data,file=file.path(filePath,fileName),row.names = FALSE,quote=TRUE)
+  
+  data<-as.data.frame(t(data))
+  if(exists("responses")){
+    responses<<-rbind(responses,data)
+  }else{
+    responses<<-data
+  }
+}
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
 
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+  
+  formData<-reactive({
+    data<-sapply(fields,function(x) input[[x]])
+    data
   })
+  
+  observeEvent(input$submitButton,{
+    
+  
+    
+    
+    #Check ID is 6 characters long
+    if (nchar(input$quantum_id)!=6){
+      foundErrors<-1
+      msg<-"Quantum ID must be 6 characters long."
+    }
+    
+    
+    #Check second letter of ID is "Q"
+   if (substring(input$quantum_id,2,2)!="Q" && substring(input$quantum_id,2,2)!="q"){
+      foundErrors<-1
+      msg<-paste(msg,"Quantum ID must have 'Q' as it's second character.")
+    }
+      
+    
+    #Check 1st character is character
+    if (is.na(as.numeric(substring(input$quantum_id,1,1)))){
+      foundErrors<-1
+      msg<-paste(msg, "first character of Quantum ID must be a letter")
+    }
+  
+    #Check 2nd character is character
+    if (is.na(as.numeric(substring(input$quantum_id,2,1)))){
+      foundErrors<-1
+      msg<-paste(msg, "Second character of Quantum ID must be a letter")
+    }
+
+    #Check 3rd character is character
+    if (is.na(as.numeric(substring(input$quantum_id,3,1)))){
+      foundErrors<-1
+      msg<-paste(msg, "Third character of Quantum ID must be a letter")
+    }
+
+    #Check 6th character is character
+    if (is.na(as.numeric(substring(input$quantum_id,6,1)))){
+      foundErrors<-1
+      msg<-paste(msg, "Sixth character of Quantum ID must be a letter")
+    }
+
+    #Check 4th and 5th characters are numbers
+    if (!is.na(as.numeric(substring(input$quantum_id,4,5)))){
+      foundErrors<-1
+      msg<-paste(msg, "Fourth & Fifth characters of Quantum ID must be numbers")
+    }
+
+    
+    if (foundErrors==1){
+      showNotification(msg,type="error")
+      output$quantum_error<-renderText({
+        "Quantum ID must be of the format: 3 Letters, 2 Numbers, 3 Letters. The second letter should be 'Q'."
+      })
+    }else{
+    saveData(formData())  
+    showNotification("Thank you. Your responses have been submitted successfully.",type="message")
+    shinyjs::reset("form")
+    
+      #  showNotification("Error: Your Quantum ID must be 3 letter, 2 digits, 1 letter.",type="error")
+    }
+  })
+
+  
 
 })
--- ui.R
+++ ui.R
@@ -1,33 +1,95 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+library(shinythemes)
 
-# Define UI for application that draws a histogram
+#dt.prisons<-as.data.table(s3tools::s3_path_to_full_df("alpha-anvil-access-tool/prisons.txt"))
+#dt.prisons<-read.table("/home/hbutchermoj/anvil-access-app/anvil-access-tool/prisons.txt",header=FALSE)
+dt.prisons<-read.delim("prisons.txt",header=FALSE)
+dt.prisons[,1]<-as.character(dt.prisons[,1])
+choice.prisons<-dt.prisons$V1
+
+# Define UI for application
 shinyUI(fluidPage(
+  theme = shinytheme("cerulean"),
+  #dashboardHeader(title="Anvil access request form",titleWidth=450),
+  useShinyjs(),
+  div(
+  id="form",
+  
 
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
 
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+  dashboardBody(
+    
+    fluidRow(
+      box(title="Anvil access request form",solidHeader = TRUE,"To gain access to the HMPPS Anvil apps, please fill in the fields below and submit the form.")
     ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+    
+    fluidRow(
+      box(
+        textInput(
+          "first_name",
+          label=h3("First Name:"))
+      )
+    ),
+      
+    fluidRow(
+      box(
+        textInput(
+          "surname",
+          label=h3("Surname:"))
+      )
+    ),
+    
+    fluidRow(
+      box(
+        pickerInput(
+          inputId = "prison",
+          label = "Select Prison:", 
+          choices<-unique(dt.prisons$V1))
+        )
+    ),
+      
+    fluidRow(
+      box(
+        textInput(
+          "role",
+          label=h3("Role:"))
+        )
+    ),
+  
+    fluidRow(
+      box(
+        textInput(
+          "quantum_id",
+          label=h3("Quantum ID:")),
+        textOutput("quantum_error")
+        )
+    ),
+  
+    fluidRow(
+    
+      box(
+      awesomeCheckboxGroup(
+        inputId = "apps_needed",
+        label = "Please select which apps you require access to", 
+        choices = c("Safety Diagnostic Tool", "Bentham", "Prisoner Categorisation"),
+        inline = TRUE,
+        status = "danger")
+      )
+    ),
+    
+    fluidRow(
+      box(
+      actionButton ("submitButton", "Submit"))
     )
   )
-))
+)
+
+
+  
+
+)
+)
+
