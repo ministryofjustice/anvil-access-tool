60b6f9b6508e416102096ac4e91afd6b83c6ef9b
hbutchermoj <hannah.butcher2@noms.gsi.gov.uk>
2018-08-28 10:53:48 +0000

    - Added loadData function to simplify code - Rearranged script and made small changes based on adding 'account status' column

--- global.R
+++ global.R
@@ -1,30 +1,25 @@
-
 # Load prison dropdown from s3
 dt.prisons <- data.table::as.data.table(s3tools::s3_path_to_full_df(
   "alpha-app-anvil-access-tool/prisons_and_offices_v2.csv", header = FALSE))
 
 
 fields <- c("first_name", "surname", "prison", "role", "quantum_id",
-            "apps_needed", "bentham", "safety", "categorisation")
+            "apps_needed", "bentham", "safety", "categorisation", "account")
 
 foundErrors <- 0
 quantumErr <- 0
-
-responses <- s3tools::s3_path_to_full_df("alpha-app-anvil-access-tool/anvil-app-responses.csv", header = TRUE)
-names(responses) <- fields[-6]
 
 tick <- "<i class=\"fa fa-check de-color\" aria-hidden=\"true\"></i>"
 cross <- "<i class=\"fa fa-times de-color\" aria-hidden=\"true\"></i>"
 
 
 saveData <- function (data) {
-
   #Reload data from S3
   responses <- data.table::as.data.table (s3tools::s3_path_to_full_df
                                           ("alpha-app-anvil-access-tool/anvil-app-responses.csv", header = TRUE))
-
+  responses <- responses[,1:9]
+  names(responses) <- fields[-6]
   data <- as.data.frame (t (data), stringsAsFactors = FALSE)
-
   if (exists ("responses")) {
     bentham_check <- as.integer ("Bentham" %in% unlist (data [6]))
     safety_check <- as.integer ("Safety Diagnostic Tool" %in% unlist (data [6]))
@@ -32,13 +27,15 @@ saveData <- function (data) {
     data$bentham <- bentham_check
     data$safety <- safety_check
     data$categorisation <- cat_check
+    data$account<- "Requested"
     data <- data [, -6]
-    responses <- rbind (responses, data)
+    responses <- rbind (responses[,1:9], data)
   }else{
     responses <- data
     responses$bentham <- as.integer ("Bentham" %in% unlist (responses [1, 6]))
     responses$safety <- as.integer ("Safety Diagnostic Tool" %in% unlist (responses [1, 6]))
     responses$categorisation <- as.integer ("Prisoner Categorisation" %in% unlist (responses [1, 6]))
+    responses$account <- "Requested"
   }
 
   #Format data and save to s3
@@ -53,4 +50,32 @@ saveData <- function (data) {
                                   "alpha-app-anvil-access-tool/anvil-app-responses.csv",
                                   overwrite = TRUE,
                                   row.names = FALSE)
+  
 }
+
+loadData <- function() {
+  responses <- data.table::as.data.table (s3tools::s3_path_to_full_df
+                                          ("alpha-app-anvil-access-tool/anvil-app-responses.csv", header = TRUE))
+  responses <- responses[,1:9]
+  names(responses) <- fields[-6]
+  # data <- as.data.frame (t (data), stringsAsFactors = FALSE)
+  responses
+}
+
+
+
+
+# Load prison dropdown from s3
+dt.prisons <- data.table::as.data.table(s3tools::s3_path_to_full_df(
+  "alpha-app-anvil-access-tool/prisons_and_offices_v2.csv", header = FALSE))
+
+
+fields <- c("first_name", "surname", "prison", "role", "quantum_id",
+            "apps_needed", "bentham", "safety", "categorisation", "account")
+
+foundErrors <- 0
+quantumErr <- 0
+
+tick <- "<i class=\"fa fa-check de-color\" aria-hidden=\"true\"></i>"
+cross <- "<i class=\"fa fa-times de-color\" aria-hidden=\"true\"></i>"
+
