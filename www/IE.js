dc9d2386d0a616fbde9cc3d35e7e5eebd10e2424
joeprinold <35369456+joeprinold@users.noreply.github.com>
2019-02-26 11:36:57 +0000

    Update IE.js

--- www/IE.js
+++ www/IE.js
@@ -6,7 +6,7 @@ function CheckIE()
   var user_agent = window.navigator.userAgent;
   var user_agent_index = ua.indexOf("MSIE ");
   
-  if (user_agent_index > 0 || browser_index != -1) {
+  if (user_agent_index > 0 || browser_index != -1 || (/MSIE \d|Trident.*rv:/.test(navigator.userAgent))) {
     
     window.alert("It looks like you're using Internet Explorer! The tool will only work in Mozilla Firefox or Google Chrome.");
   }
