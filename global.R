b2e327c9b195a0f38cc2f2d8c29d0a5c032d3aa7
joe.prinold@noms.gsi.gov.uk <joe.prinold@noms.gsi.gov.uk>
2020-01-28 14:58:23 +0000

    all apps update

--- global.R
+++ global.R
@@ -9,6 +9,7 @@ library(data.table)     ##
 library(DT)             ##
 library(shinyjs)        ##
 library(shinyalert)     ##
+library(readr)          ##
 
 ## Check if app is being opening in IE and warn if so
 ##NOT CURRENTLY WORKING.........
@@ -17,13 +18,16 @@ tags$meta("http-equiv" = "X-UA-compatible", content = "IE = edge")
 includeScript("www/IE.js")
 
 ## Load prison dropdown from s3
+# dt.prisons <- data.table::as.data.table(s3tools::s3_path_to_full_df(
+#   "alpha-app-anvil-access-tool/prisons_and_offices_v2.csv", header = FALSE))
 dt.prisons <- data.table::as.data.table(s3tools::s3_path_to_full_df(
-  "alpha-app-anvil-access-tool/prisons_and_offices_v2.csv", header = FALSE))
+  "alpha-app-anvil-access-tool/prison_area_lookup_aat.csv", header = FALSE))
 
-fields <- c("first_name", "surname", "prison", "role", "quantum_id", 
+fields <- c("first_name", "surname", "area", "prison", "role", "quantum_id", 
             "bentham", "bentham_reason", "drugs_prison", "drugs_prison_reason",
-            "safety", "categorisation", "date_requested", 
-            "account", "email")
+            "network", "network_reason", "visitors", "visitors_reason",
+            "novel_drugs", "novel_drugs_reason",
+            "date_requested", "account", "email")
 
 email_choice <- c("@noms.gsi.gov.uk", "@justice.gov.uk", "@hmps.gov.uk", "@hmcts.gov.uk",
                   "@probation.gov.uk", "@justice.gsi.gov.uk", "@digital.justice.gov.uk")
@@ -37,41 +41,61 @@ cross <- "<i class=\"fa fa-times de-color\" aria-hidden=\"true\"></i>"
 saveData <- function (data) {
   ## Reload data from S3
   responses <- as.data.table(s3tools::s3_path_to_full_df(
-    "alpha-app-anvil-access-tool/anvil-app-responses_v4.csv", header = TRUE, 
+    "alpha-app-anvil-access-tool/anvil-app-responses_v5.csv", header = TRUE, 
     col_types = list(
       col_character(), 
       col_character(), 
       col_character(), 
       col_character(), 
       col_character(), 
+      col_character(), 
+      col_integer(), 
+      col_character(), 
+      col_integer(), 
+      col_character(),
       col_integer(), 
       col_character(), 
       col_integer(), 
       col_character(), 
       col_integer(), 
-      col_integer(), 
+      col_character(), 
       col_date(), 
       col_character(), 
       col_character()
     )))
   names(responses) <- fields
   responses[date_requested == "", date_requested := NA]
+  responses[bentham == "", bentham := 0]
+  responses[bentham_reason == "" | is.null(bentham_reason) | bentham_reason == "NULL", bentham_reason := NA]
   responses[drugs_prison == "", drugs_prison := 0]
-  responses[drugs_prison_reason == "", drugs_prison_reason := NA]
+  responses[drugs_prison_reason == "" | is.null(drugs_prison_reason) | drugs_prison_reason == "NULL", drugs_prison_reason := NA]
+  responses[network == "", network := 0]
+  responses[network_reason == "" | is.null(network_reason) | network_reason == "NULL", network_reason := NA]
+  responses[visitors == "", visitors := 0]
+  responses[visitors_reason == "" | is.null(visitors_reason) | visitors_reason == "NULL", visitors_reason := NA]
+  responses[novel_drugs == "", novel_drugs := 0]
+  responses[novel_drugs_reason == "" | is.null(novel_drugs_reason) | novel_drugs_reason == "NULL", novel_drugs_reason := NA]
   responses$date_requested <- as.Date(responses$date_requested, origin = "1970-01-01")
 
   if (exists ("responses")) {
     data$bentham <- as.integer(data$bentham)
     data$drugs_prison <- as.integer(data$drugs_prison)
-    responses <- rbind(responses, data, fill = TRUE)
+    data$network <- as.integer(data$network)
+    data$visitors <- as.integer(data$visitors)
+    data$novel_drugs <- as.integer(data$novel_drugs)
+    responses <- rbind(responses, data, fill = F)
   } else {
     responses <- data
-    responses$bentham <- as.integer(data$bentham)
-    responses$drugs_prison <- as.integer(data$drugs_prison)
+    responses$bentham <- as.integer(responses$bentham)
+    responses$drugs_prison <- as.integer(responses$drugs_prison)
+    responses$network <- as.integer(responses$network)
+    responses$visitors <- as.integer(responses$visitors)
+    responses$novel_drugs <- as.integer(responses$novel_drugs)
     responses$account <- "Requested"
   }
 
   #Format data and save to s3
+  responses$area <- as.character (responses$area)
   responses$prison <- as.character (responses$prison)
   responses$role <- as.character (responses$role)
   responses$first_name <- as.character (responses$first_name)
@@ -81,10 +105,13 @@ saveData <- function (data) {
   responses$email <- as.character(responses$email)
   responses$bentham_reason <- as.character(responses$bentham_reason)
   responses$drugs_prison_reason <- as.character(responses$drugs_prison_reason)
+  responses$network_reason <- as.character(responses$network_reason)
+  responses$visitors_reason <- as.character(responses$visitors_reason)
+  responses$novel_drugs_reason <- as.character(responses$novel_drugs_reason)
   responses$date_requested <- as.Date(responses$date_requested, origin = "1970-01-01")
   
   s3tools::write_df_to_csv_in_s3 (responses,
-                                  "alpha-app-anvil-access-tool/anvil-app-responses_v4.csv",
+                                  "alpha-app-anvil-access-tool/anvil-app-responses_v5.csv",
                                   overwrite = TRUE,
                                   row.names = FALSE)
 
@@ -92,8 +119,7 @@ saveData <- function (data) {
 
 loadData <- function() {
   responses <- as.data.table(s3tools::s3_path_to_full_df(
-    "alpha-app-anvil-access-tool/anvil-app-responses_v4.csv", header = TRUE))
+    "alpha-app-anvil-access-tool/anvil-app-responses_v5.csv", header = TRUE))
   names(responses) <- fields
   responses
 }
-
--- server.R
+++ server.R
@@ -2,46 +2,72 @@
 
 ## Define server logic
 shinyServer(function(input, output, session) {
-
+  
+  output$prison_picker <- renderUI({
+    pickerInput(inputId = "prison",
+                label = NULL,
+                width = "100%",
+                selected = "",
+                choices = c("--Please Select Prison (if relevant)--", c("Whole region", unique(dt.prisons[active == T & soct_region == input$area, prison]))))
+  })
+  
   form_data <- reactive({
     data <- data.table(
-      first_name = input$first_name, 
-      surname = input$surname, 
-      prison = input$prison, 
-      role = input$role, 
-      quantum_id = input$quantum_id, 
-      bentham = input$bentham, 
-      bentham_reason = ifelse(input$bentham == 1, input$bentham_reason, NA), 
-      drugs_prison = input$drugs, 
-      drugs_prison_reason = ifelse(input$drugs == 1, input$drugs_reason, NA),  
-      safety = NA, 
-      categorisation = NA, 
-      date_requested = as.Date(Sys.Date()), 
-      email = input$email, 
+      first_name = input$first_name,
+      surname = input$surname,
+      area = input$area,
+      prison = ifelse(is.null(input$prison), NA, ifelse(input$prison == "--Please Select Prison (if relevant)--", NA, input$prison)),
+      role = input$role,
+      quantum_id = input$quantum_id,
+      bentham = input$bentham,
+      bentham_reason = ifelse(input$bentham == 1, input$bentham_reason, NA),
+      drugs_prison = input$drugs,
+      drugs_prison_reason = ifelse(input$drugs == 1, input$drugs_reason, NA),
+      network = input$network,
+      network_reason = ifelse(input$network == 1, input$network_reason, NA),
+      visitors = input$visitors,
+      visitors_reason = ifelse(input$visitors == 1, input$visitors_reason, NA),
+      novel_drugs = input$novel_drugs,
+      novel_drugs_reason = ifelse(input$novel_drugs == 1, input$novel_drugs_reason, NA),
+      date_requested = as.Date(Sys.Date()),
+      email = input$email,
       account = "Requested"
     )
   })
-  
-  responses_subset <- reactive({
+
+  responses_all <- reactive({
+    reactive_trigger <- input$area
     responses <- loadData()
-    responses[responses$prison == input$prison & (responses$bentham == 1 | responses$drugs_prison == 1),
-              c("first_name", "surname", "role", "quantum_id", 
-                "bentham", "bentham_reason", "drugs_prison", "drugs_prison_reason",
-                "safety", "categorisation", "date_requested", 
-                "account", "email")]
+    responses <- responses[(bentham == 1 | drugs_prison == 1 | network == 1 | visitors == 1 | novel_drugs == 1),
+                           c("first_name", "surname", "role", "quantum_id", 
+                             "bentham", "bentham_reason", "drugs_prison", "drugs_prison_reason",
+                             "network", "network_reason", "visitors", "visitors_reason",
+                             "novel_drugs", "novel_drugs_reason",
+                             "date_requested", "account", "email", "area")]
+    print("in all")
+    print(head(responses, 1))
+    return(responses)
   })
-  
+    
+  responses_subset <- reactive({
+    responses <- responses_all()[area == input$area & (bentham == 1 | drugs_prison == 1 | network == 1 | visitors == 1 | novel_drugs == 1)]
+    print("in subset")
+    print(head(responses, 1))
+    return(responses)
+  })
   
   output$prison_access <- renderDataTable({
     req(nrow(responses_subset()) > 0)
     access_table <- responses_subset()
     access_table$bentham <- ifelse(access_table$bentham == 1, tick, cross)
     access_table$drugs_prison <- ifelse(access_table$drugs_prison == 1, tick, cross)
+    access_table$network <- ifelse(access_table$network == 1, tick, cross)
+    access_table$visitors <- ifelse(access_table$visitors == 1, tick, cross)
+    access_table$novel_drugs <- ifelse(access_table$novel_drugs == 1, tick, cross)
     
     ## Render table but remove columns so don't display email address
     ## or reasons as makes table too wide
-
-    datatable(access_table[, c(1:5, 7, 12)], escape = FALSE,
+    datatable(access_table[, c(1:5, 7, 9, 11, 13, 16)], escape = FALSE,
               options = list(paging = FALSE,
                              scrollCollapse = T,
                              dom = "ft", 
@@ -49,18 +75,18 @@ shinyServer(function(input, output, session) {
                              scrollY = "500px"),
               rownames = FALSE,
               colnames = c("First Name", "Surname", "Role", "Quantum ID",
-                           "Bentham", "Drugs in Prisons",
+                           "Bentham", "Drugs in Prisons", "Network",
+                           "Visitors", "Novel Drugs",
                            "Account Status")
-              )
+    )
   })
-
+  
   output$prison_access_null <- renderText({
     req(!nrow(responses_subset()) > 0)
     "No users at your prison have access."
   })
-
-  #Show Bentham reason box if require access to Bentham
   
+  #Show Bentham reason box if require access to Bentham
   output$bentham_check <- renderUI({
     if (input$bentham == TRUE){
       fluidRow(column(width = 10,
@@ -74,13 +100,51 @@ shinyServer(function(input, output, session) {
     }})
   
   #Show drugs reason box if require access to drugs app
-  
   output$drugs_check <- renderUI({
     if (input$drugs == TRUE){
       fluidRow(column(width = 10,
                       div(class = "class_bentham_reason",
                           textAreaInput("drugs_reason",
                                         label = h5("If you are requesting access to the Drugs in Prisons app,
+                                              you must outline why access is required:"),
+                                        width = "100%",
+                                        rows = 6)
+                      )))
+    }})
+  
+  #Show Network reason box if require access to app
+  output$network_check <- renderUI({
+    if (input$network == TRUE){
+      fluidRow(column(width = 10,
+                      div(class = "class_bentham_reason",
+                          textAreaInput("network_reason",
+                                        label = h5("If you are requesting access to the Prison Network app,
+                                              you must outline why access is required:"),
+                                        width = "100%",
+                                        rows = 6)
+                      )))
+    }})
+  
+  #Show visitors reason box if require access to app
+  output$visitors_check <- renderUI({
+    if (input$visitors == TRUE){
+      fluidRow(column(width = 10,
+                      div(class = "class_bentham_reason",
+                          textAreaInput("visitors_reason",
+                                        label = h5("If you are requesting access to the Prison Visitor app,
+                                              you must outline why access is required:"),
+                                        width = "100%",
+                                        rows = 6)
+                      )))
+    }})
+  
+  #Show novel drugs reason box if require access to app
+  output$novel_drugs_check <- renderUI({
+    if (input$novel_drugs == TRUE){
+      fluidRow(column(width = 10,
+                      div(class = "class_bentham_reason",
+                          textAreaInput("novel_drugs_reason",
+                                        label = h5("If you are requesting access to the Novel Drug Words app,
                                               you must outline why access is required:"),
                                         width = "100%",
                                         rows = 6)
@@ -98,7 +162,7 @@ shinyServer(function(input, output, session) {
       output$first_name_err <- renderText({""})
       output$first_name_icon <- renderUI({icon("check")})
     }
-
+    
     if(nchar(input$surname) == 0) {
       output$surname_err <- renderText({"Surname must not be blank."})
       output$surname_icon <- renderUI({icon("times")})
@@ -107,7 +171,7 @@ shinyServer(function(input, output, session) {
       output$surname_err <- renderText({""})
       output$surname_icon <- renderUI({icon("check")})
     }
-
+    
     if(!grepl("@", input$email) || (grepl(",", input$email))) {
       output$email_err <- renderText({"Email address invalid."})
       output$email_icon <- renderUI({icon("times")})
@@ -116,7 +180,7 @@ shinyServer(function(input, output, session) {
       output$email_err <- renderText({""})
       output$email_icon <- renderUI({icon("check")})
     }
-
+    
     if((input$email != input$email_confirmation) || nchar(input$email_confirmation) == 0 ) {
       output$email_confirmation_err <- renderText({"Email does not match above."})
       output$email_confirmation_icon <- renderUI({icon("times")})
@@ -125,7 +189,7 @@ shinyServer(function(input, output, session) {
       output$email_confirmation_err <- renderText({""})
       output$email_confirmation_icon <- renderUI({icon("check")})
     }
-
+    
     if(nchar(input$role) == 0) {
       output$role_err <- renderText({"Role must not be blank."})
       output$role_icon <- renderUI({icon("times")})
@@ -134,48 +198,63 @@ shinyServer(function(input, output, session) {
       output$role_err <- renderText({""})
       output$role_icon <- renderUI({icon("check")})
     }
-
-    if((input$prison) == "--Please Select Prison--") {
-      output$prison_err <- renderText({"Please select a prison."})
+    
+    if((input$area) == "--Please Select Area--") {
+      output$prison_err <- renderText({"Please select an area"})
       output$prison_icon <- renderUI({icon("times")})
       foundErrors <- 1
     } else {
       output$prison_err <- renderText({""})
       output$prison_icon <- renderUI({icon("check")})
     }
-
-    if(all(is.null(input$bentham),
-           is.null(input$drugs))) {
+    
+    if(all(isFALSE(input$bentham),
+           isFALSE(input$drugs),
+           isFALSE(input$network),
+           isFALSE(input$visitors),
+           isFALSE(input$novel_drugs))) {
+      
       output$apps_err <- renderText({"Please select at least one app."})
       output$apps_icon <- renderUI({icon("times")})
       foundErrors <- 1
-    } else {
-      output$apps_err <- renderText({""})
-      output$apps_icon <- renderUI({icon("check")})
-    }
-    
-    ## check if this quantum id is already in the list
-    if(input$quantum_id %in% unlist(responses_subset()[, 4])) {
       
-      ## if any requests are made where access doesn't already exist
-      if (any((all(unlist(responses_subset()[quantum_id == input$quantum_id, 5]) == 0) &
-               input$bentham == T) |
-              (all(unlist(responses_subset()[quantum_id == input$quantum_id, 7]) == 0) &
-               input$drugs == T))) {
+      ## if apps selected, check if this quantum id is already in the list
+    } else if (input$quantum_id %in% unlist(responses_all()[, 4])) {
+      
+      ##only allow requests made for new apps i.e. where access doesn't already exists
+      bentham_already <- any(unlist(responses_all()[quantum_id == input$quantum_id, 5]) == 1) & input$bentham == T
+      drugs_already <- any(unlist(responses_all()[quantum_id == input$quantum_id, 7]) == 1) & input$drugs == T
+      network_already <- any(unlist(responses_all()[quantum_id == input$quantum_id, 9]) == 1) & input$network == T
+      visitors_already <- any(unlist(responses_all()[quantum_id == input$quantum_id, 11]) == 1) & input$visitors == T
+      novel_drugs_already <- any(unlist(responses_all()[quantum_id == input$quantum_id, 13]) == 1) & input$novel_drugs == T
+      
+      # print(any(bentham_already, drugs_already, network_already, visitors_already, novel_drugs_already))
+      
+      if (any(bentham_already, drugs_already, network_already, visitors_already, novel_drugs_already)) {
+        
+        alreadys <- c()
+        if (bentham_already) { alreadys <- c(alreadys, "<br>Seized Media Database (previously Bentham)") }
+        if (drugs_already) { alreadys <- c(alreadys, "<br>Drug Conveyancing App") }
+        if (network_already) { alreadys <- c(alreadys, "<br>Prison Network App") }
+        if (visitors_already) { alreadys <- c(alreadys, "<br>Prison Visitors App") }
+        if (novel_drugs_already) { alreadys <- c(alreadys, "<br>Novel Drug Words App") }
+        
+        output$apps_err <- renderUI({
+          HTML(paste0("This Quantum account already has access, or has requested access to, the following apps:<br>",
+                 paste(alreadys, collapse = ", "),
+                 "<br><br>Please remove those already requested, or if you think this is not the case please email intel_app_support.digital.justice.gov.uk and explain your case."))
+        })
+        output$apps_icon <- renderUI({icon("times")})
+        foundErrors <- 1
+        
+        ## apps selected and quantum id has a new set of requests
+      } else {
         
         output$apps_err <- renderText({""})
         output$apps_icon <- renderUI({icon("check")})
-        
-      ##only requests made are for apps where access exists
-      } else {
-        
-        output$apps_err <- renderText({"This Quantum account already has access
-       or has requested access. If you think this is not the case please email
-       anvil@noms.gsi.gov.uk and explain your case."})
-        output$apps_icon <- renderUI({icon("times")})
-        foundErrors <- 1
       }
       
+      ## apps selected and quantum id not on list... so requests where access doesn't already exist
     } else {
       output$apps_err <- renderText({""})
       output$apps_icon <- renderUI({icon("check")})
@@ -192,37 +271,37 @@ shinyServer(function(input, output, session) {
       foundErrors <- 1
       quantumErr <- 1
     }
-
+    
     #Check 1st character is character
     if(!is.na(as.numeric(substring(input$quantum_id, 1, 1)))) {
       foundErrors <- 1
       quantumErr <- 1
     }
-
+    
     #Check 2nd character is character
     if(!is.na(as.numeric(substring(input$quantum_id, 2, 1)))) {
       foundErrors <- 1
       quantumErr <- 1
     }
-
+    
     #Check 3rd character is character
     if(!is.na(as.numeric(substring(input$quantum_id, 3, 1)))){
       foundErrors <- 1
       quantumErr <- 1
     }
-
+    
     #Check 6th character is character
     if(!is.na(as.numeric(substring(input$quantum_id, 6, 1)))){
       foundErrors <- 1
       quantumErr <- 1
     }
-
+    
     #Check 4th and 5th characters are numbers
     if(is.na(as.numeric(substring(input$quantum_id, 4, 5)))){
       foundErrors <- 1
       quantumErr <- 1
     }
-
+    
     #Print Quantum ID validation messages
     if(quantumErr == 1){
       output$quantum_error <- renderText({"Quantum ID must be of the format:
@@ -237,21 +316,53 @@ shinyServer(function(input, output, session) {
     if(input$bentham == TRUE &&
        input$bentham_reason == "") {
       output$bentham_reason_err <- renderText({"You must provide a reason why access to the Bentham app is required."})
+      output$apps_icon <- renderUI({icon("times")})
       foundErrors <- 1
     } else {
       output$bentham_reason_err <- renderText({""})
-    }  
+    }
     
     
     # Check Drugs reason if Drugs ticked
     if(input$drugs == TRUE &&
        input$drugs_reason == "") {
       output$drugs_reason_err <- renderText({"You must provide a reason why access to the Drugs in Prisons app is required."})
+      output$apps_icon <- renderUI({icon("times")})
       foundErrors <- 1
     } else {
-      output$drug_reason_err <- renderText({""})
-    }  
-
+      output$drugs_reason_err <- renderText({""})
+    }
+    
+    # Check Network reason if network ticked
+    if(input$network == TRUE &&
+       input$network_reason == "") {
+      output$network_reason_err <- renderText({"You must provide a reason why access to the Prison Network app is required."})
+      output$apps_icon <- renderUI({icon("times")})
+      foundErrors <- 1
+    } else {
+      output$network_reason_err <- renderText({""})
+    } 
+    
+    # Check Visitors reason if Visitors ticked
+    if(input$visitors == TRUE &&
+       input$visitors_reason == "") {
+      output$visitors_reason_err <- renderText({"You must provide a reason why access to the Visitors app is required."})
+      output$apps_icon <- renderUI({icon("times")})
+      foundErrors <- 1
+    } else {
+      output$visitors_reason_err <- renderText({""})
+    } 
+    
+    # Check Novel Drugs reason if Novel Drugs ticked
+    if(input$novel_drugs == TRUE &&
+       input$novel_drugs_reason == "") {
+      output$novel_drugs_reason_err <- renderText({"You must provide a reason why access to the Novel Drug Words app is required."})
+      output$apps_icon <- renderUI({icon("times")})
+      foundErrors <- 1
+    } else {
+      output$novel_drugs_reason_err <- renderText({""})
+    } 
+    
     if(foundErrors == 1){
       #Show error message
       shinyalert("There are errors in your submission.
